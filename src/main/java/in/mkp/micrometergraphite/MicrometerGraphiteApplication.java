af8e7c88cd0bdf9a1de603bf4a85f2f3006e1c8e
Mahesh Kumar <mahesh.kumar@amobee.com>
2019-11-06 13:28:02 -0500

    first commit

---
+++ src/main/java/in/mkp/micrometergraphite/MicrometerGraphiteApplication.java
@@ -0,0 +1,13 @@
+package in.mkp.micrometergraphite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerGraphiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrometerGraphiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/mkp/micrometergraphite/MicrometerGraphiteApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mkp.micrometergraphite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerGraphiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
